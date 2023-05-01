5d581b4c2a6728bd46e0d66c606c526e0d0a4035
Pawel Jakubowski <pawel-jakubowski@hotmail.com>
2023-05-01 13:14:53 +0200

    Add translation module
    Used APIs:
    - DeepL (paid)
    - Google Translator from googletranslate-python package (free, set as
      fallback if DeepL API key is not provided)

---
+++ translate/translate.py
@@ -0,0 +1,76 @@
+"""
+This is the translation module.
+
+The translation module supplies one function, tranlate().  For example,
+
+>>> translate("Hello", "fr")
+'Bonjour'
+"""
+
+
+def translate(text: str, target_lang: str) -> str:
+    """Return a machine translation for text.
+
+    >>> translate("¿Cómo estás?", "en-gb")
+    'How are you?'
+
+    :param text: Text to be translated
+    :param target_lang: Output language
+    :return: Translated text in output language
+    """
+    import os
+    import logging
+    deepl_env_name = "DEEPL_API_KEY"
+
+    try:
+        if deepl_env_name in os.environ:
+            return deepl_translate(text, target_lang, os.environ[deepl_env_name])
+    except Exception as e:
+        logging.warning("DeepL raised an error: ", e)
+
+    try:
+        return webgoogle_translate(text, target_lang)
+    except Exception as e:
+        logging.warning("Google Translator (web) raised an error: ", e)
+
+    logging.error("Cannot provide a translation! Returning original text...")
+    return text
+
+
+def deepl_translate(text: str, target_lang: str, auth_key: str) -> str:
+    """"Return a machine translation from DeepL for text.
+
+    :param text: Text to be translated
+    :param target_lang: Output language
+    :param auth_key: DeepL API key
+    :return: Translated text in output language
+    """
+    import deepl
+    translator = deepl.Translator(auth_key)
+
+    result = translator.translate_text(text, target_lang=target_lang)
+    return result.text
+
+
+def webgoogle_translate(text: str, target_lang: str) -> str:
+    """"Return a machine translation from Google Translator using webpage for text.
+
+    >>> webgoogle_translate("Alice has a cat.", "pl")
+    'Alicja ma kota.'
+
+    :param text: Text to be translated
+    :param target_lang: Output language
+    :return: Translated text in output language
+    """
+    from googletranslatepy import Translator
+    target_lang = target_lang.lower()
+    if target_lang == "en-gb" or target_lang == "en-us":
+        target_lang = "en"
+    translator = Translator(target=target_lang)
+    return translator.translate(text)
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
