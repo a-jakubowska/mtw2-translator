1d12b77c1abaa6d48be45603ab895d8ed30e9059
Agnieszka <84205304+a-jakubowska@users.noreply.github.com>
2023-05-05 10:28:49 +0200

    Directory translation (#6)
    Translate whole directory in-place
    
    ---------
    
    Co-authored-by: Agnieszka Jakubowska <a.jakubowska.tester@gmail.com>

--- mtw2-translator.py
+++ mtw2-translator.py
@@ -1,4 +1,6 @@
-import translate.translate as t
+import translate as t
+import reference_translation as rt
+from pathlib import Path
 
 
 def translate_file(filename, newfilename, lang):
@@ -16,6 +18,32 @@ def translate_file(filename, newfilename, lang):
             new.write(line)
 
 
+def translate_dir(dirname: str, lang: str) -> None:
+    # Create Path object from string
+    dirpath = Path(dirname)
+
+    # Create directory to store translations
+    translated_dir = dirpath.parent / (dirpath.name + "-" + lang)
+    if not translated_dir.exists():
+        translated_dir.mkdir()
+
+    # Translate file-by-file
+    for orig_file in dirpath.glob('*.txt'):
+        translated_file = translated_dir / orig_file.name
+        print(f"Translating {orig_file} to {translated_file} ({lang})")
+        translate_file(orig_file, translated_file, lang)
+
+    # Set desired paths
+    orig_path = dirpath
+    backup_path = Path(str(dirpath) + "-backup")
+
+    # rename paths
+    print(f"Moving {dirpath.name} to {backup_path.name}")
+    dirpath.rename(backup_path)
+    print(f"Moving {translated_dir.name} to {orig_path.name}")
+    translated_dir.rename(orig_path)
+
+
 if __name__ == "__main__":
     langdict = t.get_available_languages()
     print("Avaliable languages:")
@@ -29,17 +57,7 @@ if __name__ == "__main__":
         else:
             break
 
+    moddir = input('Enter a mod directory:')
 
-    fname = input('Enter an original file name:')
-    nfile = input('Enter a new file name:')
-
-    try:
-        fhand = open(fname)
-    except:
-        print('File', fname, 'cannot be opened')
-        exit()
-
-    try:
-        translate_file(fname, nfile, lang)
-    except Exception as e:
-        print(f"File {fname} cannot be translated ({e})")
+    # translation
+    translate_dir(moddir, lang)
